453dbe960f6e9900280e2dde4c9fb628b55fe2c4
itays123 <itays2005@gmail.com>
2022-04-22 01:18:16 +0300

    PATCH: Add global definitions of datetime

--- src/datetime.ts
+++ src/datetime.ts
@@ -1,2 +1,5 @@
 export type DayOfWeek = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 export type HourOfDay = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+
+export const DAYS_IN_WEEK = 7;
+export const HOURS_OF_DAY = 13;
